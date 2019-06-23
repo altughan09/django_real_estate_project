a162648349fff7bd94837b3fa552be2b63655bd5
Altughan Ozengi <31768522+Blockeer09@users.noreply.github.com>
2019-06-23 13:12:33 +0200

    loading effect added

--- mysite/static/pages/js/main.js
+++ mysite/static/pages/js/main.js
@@ -4,3 +4,17 @@ document.querySelector('.year').innerHTML = date.getFullYear();
 setTimeout(function(){
     $('#message').fadeOut('slow');
 }, 3000);
+
+$(document).ready(function() {
+    $('#selector').select2({
+        placeholder: "City (All)",
+    });
+});
+
+function hideLoader() {
+    $('#loading').fadeOut("slow");
+}
+
+$(window).ready(hideLoader);
+
+setTimeout(hideLoader, 5000);
